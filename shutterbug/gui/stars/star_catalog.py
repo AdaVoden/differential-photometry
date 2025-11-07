55f6c46c5fa453839d23f199ce10fc5c27074451
Ada Voden <ada@voden.ca>
2025-11-07 13:00:00 -0700

    added code to StarCatalog and StarManager

--- shutterbug/gui/stars/star_catalog.py
+++ shutterbug/gui/stars/star_catalog.py
@@ -1,6 +1,8 @@
 import logging
 
-from .star import StarIdentity
+from .star import StarIdentity, StarMeasurement
+
+from scipy.spatial import KDTree
 
 
 class StarCatalog:
@@ -15,23 +17,54 @@ class StarCatalog:
             # initial variables
             self.stars = {}  # id -> StarIdentity
             self._kdtree = None
-            self._coords = []
-            self._ids = []
+            self._coords = []  # Reference coordinates
+            self._ids = []  # Matching star IDs
             self._dirty = False
         return cls._instance
 
     def add_star(self, star_identity: StarIdentity, x: float, y: float):
         """Adds a star to the catalog"""
-        pass
+        self.stars[star_identity.id] = star_identity
+        self._coords.append((x, y))
+        self._ids.append(star_identity.id)
+        self._dirty = True
 
     def _ensure_tree(self):
         """Rebuilds the KDTree for spacial coordinates"""
-        pass
+        if self._kdtree is None or self._dirty is True:
+            # Generate new tree!
+            self._kdtree = KDTree(self._coords)
+        self._dirty = False
 
-    def find_near(self, x: float, y: float, tolerance: float = 3.0):
+    def find_nearest(self, x: float, y: float, tolerance: float = 3.0):
         """Finds stars, if any, that exist at x/y position within tolerance"""
-        pass
+        self._ensure_tree()
 
-    def register_measurement(self, x: float, y: float, image_name: str):
+        dist, idx = self._kdtree.query((x, y), distance_upper_bound=tolerance)
+
+        if idx == self._kdtree.n:
+            # Found nothing
+            return None
+        else:
+            return self._ids[idx]
+
+    def _new_id(self) -> int:
+        if not self._ids:
+            # No other stars available
+            return 1
+        last_id = self._ids[-1]
+        return last_id + 1
+
+    def register_measurement(
+        self, measurement: StarMeasurement, is_target: bool, image_name: str
+    ) -> StarIdentity:
         """Registers measurement with catalog"""
-        pass
+        match_id = self.find_nearest(measurement.x, measurement.y)
+        if match_id:
+            star = self.stars[match_id]
+        else:
+            star_id = f"star_{self._new_id}"
+            star = StarIdentity(star_id, is_target)
+            self.add_star(star, measurement.x, measurement.y)
+        star.measurements[image_name] = measurement
+        return star
--- shutterbug/gui/stars/star_manager.py
+++ shutterbug/gui/stars/star_manager.py
@@ -3,6 +3,8 @@ from PySide6.QtCore import Signal, QObject
 from .star import StarMeasurement
 from .star_catalog import StarCatalog
 
+from scipy.spatial import KDTree
+
 import logging
 
 
@@ -16,26 +18,44 @@ class StarManager(QObject):
     def __init__(self):
         super().__init__()
 
-        self.stars = []  # Master star list
-        self.star_coords = {}  # (x, y) -> StarMeasurement
+        self.star_coords = []  # Master coord list
+        self.stars = {}  # (x, y) -> StarMeasurement
         self._kdtree = None  # Spatial coordinate for lookups
         self._dirty = False
         self.catalog = StarCatalog()  # Singleton
 
     def add_star(self, star: StarMeasurement):
         """Add star measurement to manager"""
-        self.stars.append(star)
-        self.star_coords[(round(star.x), round(star.y))] = star
+        coords = (round(star.x), round(star.y))
+        self.stars[coords] = star
+        self.star_coords.append((star.x, star.y))
+        self._dirty = True
         self.star_added.emit(star)
 
     def remove_star(self, star: StarMeasurement):
         """Remove star measurement from manager"""
-        pass
+        coords = (round(star.x), round(star.y))
+        self.star_coords.remove((star.x, star.y))
+        self.stars.pop(coords)
+        self._dirty = True
+        self.star_removed.emit(star)
 
     def _ensure_tree(self):
         """Create or rebuild the KDTree for spacial detection"""
-        pass
+        if self._kdtree is None or self._dirty is True:
+            # Generate new tree!
+            self._kdtree = KDTree(self.star_coords)
+        self._dirty = False
 
     def find_nearest(self, x: float, y: float, tolerance: float = 3.0):
         """Finds the nearest star from coordinate"""
-        pass
+        self._ensure_tree()
+        dist, idx = self._kdtree.query((x, y), distance_upper_bound=tolerance)
+
+        if idx == self._kdtree.n:
+            # Found nothing
+            return None
+        else:
+            pure_coords = self.star_coords[idx]
+            coords = (round(pure_coords[0]), round(pure_coords[1]))
+            return self.stars[coords]
