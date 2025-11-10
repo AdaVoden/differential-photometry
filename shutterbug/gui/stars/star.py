02eadb4663a9dc7ca21c32d69aba828e0b5b1862
Ada Voden <ada@voden.ca>
2025-11-09 18:54:41 -0700

    Removed pydantic, made Star dataclasses observable

--- shutterbug/gui/stars/star.py
+++ shutterbug/gui/stars/star.py
@@ -1,44 +1,72 @@
-from pydantic import BaseModel
-
 from PySide6.QtCore import QObject, Signal
 
 from typing import Dict, Optional
 
 
-class StarMeasurement(QObject, BaseModel):
-    """Measurement of a star within an image"""
+class ObservableQObject(QObject):
+    """Dataclass-like observable QObject"""
 
     updated = Signal(object)
 
-    # Intrinsic
-    x: float
-    y: float
-    time: float
-    image: str
-    # Calculated from measurement
-    flux: Optional[float] = None
-    flux_error: Optional[float] = None
-    fwhm: Optional[float] = None
-    mag: Optional[float] = None
-    mag_error: Optional[float] = None
+    def _define_field(self, name, default):
+        private_name = f"_{name}"
+        setattr(self, private_name, default)
 
-    def __init__(self, **kwargs):
-        QObject.__init__(self)
-        BaseModel.__init__(self, **kwargs)
+        def getter(self):
+            return getattr(self, private_name)
 
-    def __setattr__(self, name, value):
-        # Pydantic can still do its thing
-        old_value = getattr(self, name, None)
-        super().__setattr__(name, value)
-        # And we can emit a signal when changed
-        if name in StarMeasurement.model_fields and old_value != value:
-            self.updated.emit(self)
+        def setter(self, value):
+            if value != getattr(self, private_name):
+                setattr(self, private_name, value)
+                self.updated.emit(self)
+
+        setattr(self.__class__, name, property(getter, setter))
 
 
-class StarIdentity(BaseModel):
+class StarMeasurement(ObservableQObject):
+    """Measurement of a star within an image"""
+
+    def __init__(
+        self,
+        x: float,
+        y: float,
+        time: float,
+        image: str,
+        flux: Optional[float] = None,
+        flux_error: Optional[float] = None,
+        mag: Optional[float] = None,
+        mag_error: Optional[float] = None,
+    ):
+        super().__init__()
+        # Intrinsic
+        self.x = x
+        self.y = y
+        self.time = time
+        self.image = image
+        # Computed later
+        self.flux = self._define_field("flux", flux)
+        self.flux_error = self._define_field("flux_error", flux_error)
+        self.mag = self._define_field("mag", mag)
+        self.mag_error = self._define_field("mag_error", mag_error)
+
+
+class StarIdentity(ObservableQObject):
     """Identity of star for cross-image synchronization"""
 
     id: str
     measurements: Dict[str, StarMeasurement] = {}  # Image name -> StarMeasurement
     use_in_ensemble: bool = True
     label: Optional[str] = None
+
+    def __init__(
+        self,
+        id: str,
+        measurements: Dict[str, StarMeasurement] = {},
+        use_in_ensemble: bool = True,
+        label: Optional[str] = None,
+    ):
+        super().__init__()
+        self.id = id
+        self.measurements = measurements
+        self.use_in_ensemble = use_in_ensemble
+        self.label = label
