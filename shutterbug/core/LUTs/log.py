3a662995fcf5ca67da8b3517f15f47c89fd13238
Ada Voden <ada@voden.ca>
2025-11-30 16:07:11 -0700

    Added linear and logarithmic luts

--- shutterbug/core/LUTs/log.py
+++ shutterbug/core/LUTs/log.py
@@ -1,1 +1,19 @@
-#!/usr/bin/env python3
+import numpy as np
+from .base_stretch import BaseStretch
+from .registry import register_stretch
+
+
+@register_stretch("log")
+class LogStretch(BaseStretch):
+    def build_lut(self, low, high) -> np.ndarray:
+        if high <= low:
+            return np.arange(256, dtype=np.uint8)
+
+        x = np.linspace(0, 1, 256)
+        norm = np.clip((x - low) / (high - low), 0, 1)
+
+        # Add epsilon to avoid log(0)
+        # eps = 1e-6
+        y = np.log1p(norm * 100) / np.log1p(100)
+
+        return (y * 255).astype(np.uint8)
